16de5ded5fbce150679c47b5ff9990700e92cac0
CRPS <cristobits0101@gmail.com>
2023-10-23 15:50:27 +0100

    Resolviendo conflictos en el archivo recibe_datos

--- controllers/recibe_datos.php
+++ controllers/recibe_datos.php
@@ -1,11 +1,7 @@
 <?php
-<<<<<<< HEAD:controllers/recibe_datos.php
-   // Añadimos las validaciones al fichero de control de datos.
-   include_once "../validations/funciones_validacion.php";
-=======
+
      // Añadimos las validaciones al fichero de control de datos.
      include_once "./funciones_validacion.php";
->>>>>>> main:recibe_datos.php
 
      // Si el formulario enviado contiene el identificador oculto Cristo, se ejecuta la validación correspondiente.
      if ($_REQUEST['postId'] === "Cristo")
@@ -22,4 +18,5 @@
      {
           validate_form_manuel();
      }
+     
 ?>
