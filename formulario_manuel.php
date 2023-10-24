21b88423ff7f39511d731bdf58e93370e9f845b2
Manuel Castillo Casañas <manuelcastillocasanas@alumno.ieselrincon.es>
2023-10-24 22:42:40 +0100

    Modifico mi formulario

--- formulario_manuel.php
+++ formulario_manuel.php
@@ -1,11 +1,6 @@
 <?php
-
-require_once "./controllers/manuel_lee_fichero.php";
-
-manuel_lee_fichero();
-
-require_once "./validations/manuel_valida_fichero.php";
-
+    require_once "./controllers/manuel_lee_fichero.php";
+    $valores_defecto = manuel_lee_fichero();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,8 +9,6 @@ require_once "./validations/manuel_valida_fichero.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Título del documento -->
     <title>Formularios UT2</title>
-    <!-- Estilos del formulario -->
-    <link rel="stylesheet" href="./styles/manuel_estilo_formularios.css">
 </head>
 <body>
     <!-- Contenedor del formulario -->
@@ -28,17 +21,17 @@ require_once "./validations/manuel_valida_fichero.php";
             <!-- Input de texto -->
             <div>
                 <label for="name">Nombre:</label>
-                <input type="text" id="name" name="name" value="<?php if(valida("name", "Manuel", $valores_defecto)) echo $valores_defecto["name"]; ?>">
+                <input type="text" id="name" name="name" value="<?php echo $valores_defecto['name'] ?>">
             </div>
             <br/>
             <div>
                 <label for="password">Contraseña:</label>
-                <input type="password" id="password" name="password" value="<?php if(valida("password", "12345", $valores_defecto)) echo $valores_defecto['password'] ?>">
+                <input type="password" id="password" name="password" value="<?php echo $valores_defecto['password'] ?>">
             </div>
             <br/>
             <div>
                 <label for="repeatPassword">Repita su contraseña:</label>
-                <input type="password" id="repeatPassword" name="repeatPassword" value="<?php if (valida("repeatPassword", "12345", $valores_defecto)) echo $valores_defecto['repeatPassword'] ?>">
+                <input type="password" id="repeatPassword" name="repeatPassword" value="<?php echo $valores_defecto['repeatPassword'] ?>">
             </div>
             <br/>
             <!-- Input checkbox -->
@@ -46,17 +39,17 @@ require_once "./validations/manuel_valida_fichero.php";
                 <legend><b>Selecciona el tipo de transporte:</b></legend>
                 <br/>
                 <div>
-                    <input type="checkbox" id="aereo" value="aereo" <?php if(valida("aereo", "aereo", $valores_defecto)) echo "checked" ?> name="transp[]" />
+                    <input type="checkbox" id="aereo" value="aereo" <?php if($valores_defecto['checkTransp'] == "aereo") echo "checked" ?> name="transp[]" />
                     <label for="aereo" class="checkbox">Aéreo</label>
                 </div>
                 <br/>
                 <div>
-                    <input type="checkbox" id="maritimo" value="maritimo" <?php if(valida("maritimo", "maritimo", $valores_defecto)) echo "checked" ?> name="transp[]"/>
+                    <input type="checkbox" id="maritimo" value="maritimo" <?php if($valores_defecto['checkTransp'] == "maritimo") echo "checked" ?> name="transp[]"/>
                     <label for="maritimo" class="checkbox">Marítimo</label>
                 </div>
                 <br/>
                 <div>
-                    <input type="checkbox" id="terrestre" value="terrestre" <?php if(valida("terrestre", "terrestre", $valores_defecto)) echo "checked" ?> name="transp[]" />
+                    <input type="checkbox" id="terrestre" value="terrestre" <?php if($valores_defecto['checkTransp'] == "terrestre") echo "checked" ?> name="transp[]" />
                     <label for="terrestre" class="checkbox">Terrestre</label>
                 </div>
                 <br/>
@@ -67,17 +60,17 @@ require_once "./validations/manuel_valida_fichero.php";
                 <legend><b>Selecciona el tipo de repuesto:</b></legend>
                 <br/>
                 <div>
-                    <input type="radio" id="rotables" value="rotables" <?php if(valida("rotables", "rotables", $valores_defecto)) echo "checked" ?> name="spare[]"/>
+                    <input type="radio" id="rotables" value="rotables" <?php if($valores_defecto['radioBut'] === "rotables") echo "checked" ?> name="spare[]"/>
                     <label for="rotables" class="radio">Rotables</label>
                 </div>
                 <br/>
                 <div>
-                    <input type="radio" id="consumibles" value="consumibles" <?php if(valida("consumibles", "consumibles", $valores_defecto)) echo "checked" ?> name="spare[]" />
+                    <input type="radio" id="consumibles" value="consumibles" <?php if($valores_defecto['radioBut'] === "consumibles") echo "checked" ?> name="spare[]" />
                     <label for="consumibles" class="radio">Consumibles</label>
                 </div>
                 <br/>
                 <div>
-                    <input type="radio" id="utileria" value="utileria" <?php if(valida("utileria", "utileria", $valores_defecto)) echo "checked" ?> name="spare[]" />
+                    <input type="radio" id="utileria" value="utileria" <?php if($valores_defecto['radioBut'] === "utileria") echo "checked" ?> name="spare[]" />
                     <label for="utileria" class="radio">Utilería</label>
                 </div>
                 <br/>
@@ -87,48 +80,12 @@ require_once "./validations/manuel_valida_fichero.php";
             <div class="entrega">
                 <p><b>Entrega:<span>&nbsp;</b></span></p>
                 <select name="entrega" title="Lugar de entrega">
-                    <option value="Origen" <?php if(valida("entrega", "origen", $valores_defecto)) echo "selected" ?>>Origen</option>
-                    <option value="Destino" <?php if(valida("entrega", "destino", $valores_defecto)) echo "selected" ?>>Destino</option>
+                    <option value="Origen" <?php if($valores_defecto['entregaSelect'] === "origen") echo "selected" ?>>Origen</option>
+                    <option value="Destino" <?php if($valores_defecto['entregaSelect'] === "destino") echo "selected" ?>>Destino</option>
                 </select>
             </div>
             <input type="hidden" id="postId" name="postId" value="Manuel" />
             <br/>
-
-            <!-- Input type file para documentos -->
-            <div>
-                    <label for="fichero1">Selecciona un documento: </label>
-                    <input
-                        id="fichero1" 
-                        type="file" 
-                        name="fichero1"
-                        accept=".txt,.pdf,.docx,.xlsx,.pptx,.odt" 
-                    />
-                </div>
-
-                <br/>
-
-                <!-- Input type file para imágenes -->
-                <div>
-                    <label for="foto">Selecciona una foto: </label>
-                    <input 
-                        type="file" 
-                        id="foto"
-                        name="foto"
-                        accept=".jpg,.gif,.png,.jfif"
-                    />
-                </div>
-
-
-                <input 
-                    type="hidden" 
-                    id="postId" 
-                    name="postId" 
-                    value="Cristo" 
-                />
-
-                <br/><br/><br/>
-
-
             <!-- Botón que envía el formulario a ser validado -->
             <div style="display: flex;">
                 <input type="submit" value="Enviar" class="enviar">
